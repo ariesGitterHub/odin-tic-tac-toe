a79e2932737b5b90a77cf6ba3762c088a10fac8b
ariesGitterHub <149708069+ariesGitterHub@users.noreply.github.com>
2024-06-13 14:22:49 -0400

    game markers working, need to better understand code here

--- javascript copy 5.js
+++ javascript copy 5.js
@@ -19,6 +19,7 @@ const numPlayersFontColorModule = (function () {
   };
 })();
 
+
 // Module for handling the player modal display
 const playerTwoSelectModule = (function () {
   const numPlayers = document.querySelector("#num-players");
@@ -37,6 +38,7 @@ const playerTwoSelectModule = (function () {
     showPlayerTwoSelect,
   };
 })();
+
 
 // Module for handling the zodiac button images
 const zodBtnImgsModule = (function () {
@@ -61,6 +63,16 @@ const zodBtnImgsModule = (function () {
       button.addEventListener("mouseleave", hideImg);
       button.addEventListener("focus", showImg);
       button.addEventListener("blur", hideImg);
+
+      // button.addEventListener("mouseenter", () => {
+      //   zodSym.style.display = "none";
+      //   zodImg.style.display = "flex";
+      // });
+
+      // button.addEventListener("mouseleave", () => {
+      //   zodSym.style.display = "flex";
+      //   zodImg.style.display = "none";
+      // });
     });
   }
 
@@ -71,6 +83,116 @@ const zodBtnImgsModule = (function () {
 
 
 // Factory function for creating player selectors
+
+// (function () {
+//   function PlayerSignSelectModule(playerId, valueChecker) {
+//     const zodBtns = document.querySelectorAll(".zod-btn");
+//     const playerSelects = document.querySelector(playerId);
+//     let selectedSign = "";
+//     let selectedSignId = "";
+
+//     function selectPlayerSign() {
+//       zodBtns.forEach((button) => {
+//         button.addEventListener("click", () => {
+//           const matchingText = valueChecker.filter((text) =>
+//             text.hasOwnProperty(button.id)
+//           );
+
+//           if (matchingText.length > 0) {
+//             selectedSign = matchingText.map((obj) => obj[button.id]).join("");
+//             selectedSignId = button.id;
+//             playerSelects.textContent = selectedSign;
+//             playerSelects.style.color = "var(--red)";
+//           }
+//         });
+//       });
+//     }
+
+//     function getSelectedSign() {
+//       return selectedSign;
+//     }
+
+//     function getSelectedSignId() {
+//       return selectedSignId;
+//     }
+
+//     return {
+//       selectPlayerSign,
+//       getSelectedSign,
+//       getSelectedSignId,
+//     };
+//   }
+
+//   const valueChecker1 = [
+//     { ari1: "Aries" },
+//     { tau1: "Taurus" },
+//     { gem1: "Gemini" },
+//     { can1: "Cancer" },
+//     { leo1: "Leo" },
+//     { vir1: "Virgo" },
+//     { lib1: "Libra" },
+//     { sco1: "Scorpio" },
+//     { sag1: "Sagittarius" },
+//     { cap1: "Capricorn" },
+//     { aqu1: "Aquarius" },
+//     { pis1: "Pisces" },
+//   ];
+//   const valueChecker2 = [
+//     { ari2: "Aries" },
+//     { tau2: "Taurus" },
+//     { gem2: "Gemini" },
+//     { can2: "Cancer" },
+//     { leo2: "Leo" },
+//     { vir2: "Virgo" },
+//     { lib2: "Libra" },
+//     { sco2: "Scorpio" },
+//     { sag2: "Sagittarius" },
+//     { cap2: "Capricorn" },
+//     { aqu2: "Aquarius" },
+//     { pis2: "Pisces" },
+//   ];
+
+//   const player1SignSelect = PlayerSignSelectModule(
+//     "#player1-sign-select",
+//     valueChecker1
+//   );
+
+//   const player2SignSelect = PlayerSignSelectModule(
+//     "#player2-sign-select",
+//     valueChecker2
+//   );
+
+//   player1SignSelect.selectPlayerSign();
+//   player2SignSelect.selectPlayerSign();
+
+//   // Expose the modules globally for access in other scripts/modules...THI IS NEW FOR ME...
+//   window.player1SignSelect = player1SignSelect;
+//   window.player2SignSelect = player2SignSelect;
+
+//   // document.querySelector("#start-btn").addEventListener("click", () => {
+  
+//   function createPlayerVariables() {
+//     zodBtns.forEach((button) => {
+//         button.addEventListener("click", () => {
+//           const playerOneSign = player1SignSelect.getSelectedSign();
+//           console.log(playerOneSign);
+
+//           const playerOneSignId = player1SignSelect.getSelectedSignId();
+//           console.log(playerOneSignId);
+
+//           const playerTwoSign = player2SignSelect.getSelectedSign();
+//           console.log(playerTwoSign);
+
+//           const playerTwoSignId = player2SignSelect.getSelectedSignId();
+//           console.log(playerTwoSignId);
+//        })
+//       })
+//     };
+//     return {
+//       createPlayerVariables,
+//     };
+// })()
+
 (function () {
   // function PlayerSignSelectModule(playerId, valueChecker) {
   function PlayerSignSelectModule(playerId, playerKey) {
@@ -115,6 +237,36 @@ const zodBtnImgsModule = (function () {
     };
   }
 
+  // const valueChecker1 = [
+  //   { ari1: "Aries" },
+  //   { tau1: "Taurus" },
+  //   { gem1: "Gemini" },
+  //   { can1: "Cancer" },
+  //   { leo1: "Leo" },
+  //   { vir1: "Virgo" },
+  //   { lib1: "Libra" },
+  //   { sco1: "Scorpio" },
+  //   { sag1: "Sagittarius" },
+  //   { cap1: "Capricorn" },
+  //   { aqu1: "Aquarius" },
+  //   { pis1: "Pisces" },
+  // ];
+
+  // const valueChecker2 = [
+  //   { ari2: "Aries" },
+  //   { tau2: "Taurus" },
+  //   { gem2: "Gemini" },
+  //   { can2: "Cancer" },
+  //   { leo2: "Leo" },
+  //   { vir2: "Virgo" },
+  //   { lib2: "Libra" },
+  //   { sco2: "Scorpio" },
+  //   { sag2: "Sagittarius" },
+  //   { cap2: "Capricorn" },
+  //   { aqu2: "Aquarius" },
+  //   { pis2: "Pisces" },
+  // ];
+
 const valueChecker = {
   player1: {
     ari1: "Aries",
@@ -146,6 +298,15 @@ const valueChecker = {
   },
 };
 
+  // const playerOneSignSelect = PlayerSignSelectModule(
+  //   "#player-one-sign-select",
+  //   valueChecker1
+  // );
+  // const playerTwoSignSelect = PlayerSignSelectModule(
+  //   "#player-two-sign-select",
+  //   valueChecker2
+  // );
+
     const playerOneSignSelect = PlayerSignSelectModule(
       "#player-one-sign-select",
       "player1"
@@ -174,6 +335,7 @@ numPlayersFontColorModule.changeNumPlayerFontColor();
 playerTwoSelectModule.showPlayerTwoSelect();
 zodBtnImgsModule.changeZodBtnImgs();
 
+
 const clearBtnModule = (function () {
   const clearBtn = document.querySelectorAll(".clear-btn");
 
@@ -197,6 +359,8 @@ const modalStartBtnModule = (function () {
   const alertMsg2 = document.querySelector("#alert-msg2");
   const dialog = document.querySelector("dialog");  
   const gameContainer = document.querySelector("#game-container");
+
+  
 
   function useStartBtn() {
     const playerOneSignId = window.playerOneSignSelect.getSelectedSignId();
@@ -246,7 +410,6 @@ const modalStartBtnModule = (function () {
     gameContainer.style.display = "flex";
     console.log("Starting game with:", { playerOneSignId, playerOneSign, playerTwoSignId, playerTwoSign });
 
- // Call another function with the selected sign data
     playerBoardModule.setPlayerBoard(
       playerOneSignId,
       playerOneSign,
@@ -255,6 +418,10 @@ const modalStartBtnModule = (function () {
     );
 
   }
+
+  // Call another function with the selected sign data
+
+  
 
   startBtn.addEventListener("click", useStartBtn);
 
@@ -267,6 +434,45 @@ const modalStartBtnModule = (function () {
 
 })();
 
+
+// const modalStartBtnModule = (function () {
+//   const startBtn = document.querySelector("#start-btn");
+//   const dialog = document.querySelector("dialog");
+//   const numPlayers = document.querySelector("#num-players");
+
+//   // const playerOneSign = window.player1SignSelect.getSelectedSign();
+//   const playerOneSignId = window.player1SignSelect.createPlayerVariables();
+
+//   console.log(playerOneSignId);
+
+//   // const playerTwoSign = window.player2SignSelect.getSelectedSign();
+//   const playerTwoSignId = window.player2SignSelect.createPlayerVariables();
+
+//   console.log(playerTwoSignId);
+
+//   function useStartBtn() {
+
+//     if (!numPlayers.checked && playerOneSignId === "") {
+//       alert("Please select a sign for player one.")
+//       return
+//     } else {
+//       dialog.style.display = "none";
+//     }
+
+//     if (numPlayers.checked && (playerOneSignId === "" || playerTwoSignId === "")) {
+//       alert("Please select a sign for both players.");
+//       return
+//     } else {
+//       dialog.style.display = "none";
+//     }
+
+//   }
+
+//   startBtn.addEventListener("click", useStartBtn);
+
+// })()
+
+
 const playerBoardModule = (function () {
   const playerOneMarker = document.querySelector("#player-one-marker");
   const playerOneName = document.querySelector("#player-one-name");
@@ -274,11 +480,6 @@ const playerBoardModule = (function () {
   const playerTwoMarker = document.querySelector("#player-two-marker");
   const playerTwoName = document.querySelector("#player-two-name");
   const playerTwoType = document.querySelector("#player-two-type");
-  const randomPick = Math.floor(Math.random() * 12);
-
-  const computerPlayerTwoIdList = ["ari2", "tau2", "gem2", "can2", "leo2", "vir2", "lib2", "sco2", "sag2", "cap2", "aqu2", "pis2"];
-
-  const computerPlayerTwoList = ["Aries", "Taurus", "Gemini", "Cancer", "Leo", "Virgo", "Libra", "Scorpio", "Sagittarius", "Capricorn", "Aquarius", "Pisces"];
 
   function setPlayerBoard(
     playerOneSignId,
@@ -330,6 +531,9 @@ const playerBoardModule = (function () {
       playerTwoMarker.src = playerTwoMarkerInfo.markerImg;
       playerTwoMarker.style.backgroundColor = playerTwoMarkerInfo.markerBkg;
     } else {
+      const randomPick = Math.floor(Math.random() * 12);
+      const computerPlayerTwoIdList = ["ari2", "tau2", "gem2", "can2", "leo2", "vir2", "lib2", "sco2", "sag2", "cap2", "aqu2", "pis2"];
+      const computerPlayerTwoList = ["Aries", "Taurus", "Gemini", "Cancer", "Leo", "Virgo", "Libra", "Scorpio", "Sagittarius", "Capricorn", "Aquarius", "Pisces"];
       const computerPlayerTwoId = computerPlayerTwoIdList[randomPick];
       playerTwoMarkerInfo = signMarkers[computerPlayerTwoId];
       const computerPlayerTwo = computerPlayerTwoList[randomPick];
@@ -338,10 +542,7 @@ const playerBoardModule = (function () {
       playerTwoMarker.src = playerTwoMarkerInfo.markerImg;
       playerTwoMarker.style.backgroundColor = playerTwoMarkerInfo.markerBkg;
     }
-    return {
-      playerOneMarker,
-      playerTwoMarker,
-    };
+
   }
 
   return {
@@ -350,59 +551,8 @@ const playerBoardModule = (function () {
 })();
 
 playerBoardModule.setPlayerBoard();
-// playerBoardModule.setPlayerMarker();
 
-
-// function setPlayerMarker(playerOneMarker, playerTwoMarker) {
-//   if (playerOneMarker) {
-//     const p1 = playerOneMarker.src;
-//     console.log(p1);
-//     const p1Bkg = playerOneMarker.style.backgroundColor;
-//     console.log(p1Bkg);
-//   }
-
-//   if (playerTwoMarker) {
-//     const p2 = playerTwoMarker.src;
-//     console.log(p2);
-//     const p2Bkg = playerTwoMarker.style.backgroundColor;
-//     console.log(p2Bkg);
-//   }
-// }
-
-// setPlayerMarker();
-
-function createBoardMarkers(playerOneMarker, playerTwoMarker) {
- 
-  const boardCells = document.querySelectorAll(".cell"); 
-
-  let currentPlayer = "playerOne";
-
-  boardCells.forEach((cell) => {
-    cell.addEventListener("click", function () {
-      if (!cell.innerHTML) {
-        // Check if the cell is empty first...
-        if (currentPlayer === "playerOne") {
-          cell.innerHTML = `<img src="${playerOneMarker.src}" style="background-color: ${playerOneMarker.style.backgroundColor};" />`;
-          currentPlayer = "playerTwo";
-        } else {
-          cell.innerHTML = `<img src="${playerTwoMarker.src}" style="background-color: ${playerTwoMarker.style.backgroundColor};" />`;
-          currentPlayer = "playerOne";
-        }
-      }
-    });
-  });
-}
-
-const { playerOneMarker, playerTwoMarker } = playerBoardModule.setPlayerBoard( // It can be any two items from the 24, establishes the pattern...need to see more on this...
-  "ari1",
-  "Aries",
-  "ari2",
-  "Aries"
-);
-createBoardMarkers(playerOneMarker, playerTwoMarker);
-
-
-const gameBoardModule = (function () {
+const gameBoard = (function () {
   function Cell() {
     let marker = "";
     return { marker };
@@ -421,6 +571,7 @@ const gameBoardModule = (function () {
   ];
 
   // return board
+
   function updateCell(index, newMarker) {
     if (index >= 0 && index < 9) {
       board[index].marker = newMarker;
@@ -440,15 +591,15 @@ const gameBoardModule = (function () {
   return { board, updateCell, displayCell };
 })();
 
-// gameBoardModule.updateCell(0, "E");
-// gameBoardModule.updateCell(1, "Y");
-// gameBoardModule.updateCell(2, "A");
-// console.log(gameBoardModule.updateCell(6, "X"));
-// console.log(gameBoardModule.updateCell(1, "O"));
-// console.log(gameBoardModule.updateCell(2, "X"));
-// console.log(gameBoardModule.updateCell(4, "X"));
-// console.log(gameBoardModule.updateCell(8, "O"));
-gameBoardModule.displayCell();
+// gameBoard.updateCell(0, "E");
+// gameBoard.updateCell(1, "Y");
+// gameBoard.updateCell(2, "A");
+console.log(gameBoard.updateCell(6, "X"));
+console.log(gameBoard.updateCell(1, "O"));
+console.log(gameBoard.updateCell(2, "O"));
+console.log(gameBoard.updateCell(4, "X"));
+console.log(gameBoard.updateCell(8, "X"));
+gameBoard.displayCell();
 
 // SET UP THE GAMEPLAY
 
